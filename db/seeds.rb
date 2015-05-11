b9cb9db2277f7cb455a315f604dd78beb7f9b8a9
lyman wong <lymanwong@gmail.com>
2015-05-11 12:24:21 -0700

    changed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ array = [1,2,3,4,5,6,7,8,9,10,11]
 workoutName = ["Dumbell", "Jumping Jacks", "Sprints", "Curls"]
 bodyPart = ["Arms", "Legs", "Back", "Neck", "Chest", "Abs"]
 title = ["title 1","title 2","title 3","title 4","title 5","title 6","title 7","title 8","title 9","title 10","title 11","title 12","title 13","title 14","title 15"]
-comment = ["comment 1","comment 2","comment 3","comment 4","comment 5","comment 6","comment 7","comment 8","comment 9","comment 10","comment 11","comment 12","comment 13","comment 14","comment 15"]
+comment = ["This workout was fun!","This workout was tough!","This workout was average!","This workout was crazy!","This workout was insane!","This workout was boring!","This workout was awful!","This workout was meh!","This workout was ok!","This workout was dope!","This workout was tiring!","This workout was exhausting!","This workout was super duper!","This workout was insanity!","This workout was fast!"]
 User.create(username: "Lyman", email: "lymanwong@gmail.com", password: "123")
 
 20.times do
